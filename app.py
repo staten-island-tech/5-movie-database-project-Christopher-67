93f1bef4a0346be007b64a2a98ff0c174c73c91d
Christopher-67 <christophers892@nycstudents.net>
2025-11-06 09:39:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,6 @@ movies = open("./movies.json", encoding="utf8")
 
 data = json.load(movies)
 
-for index, 
+print("title of movies")
+for index, item in enumerate(movies):
+    print(index, ":",item["title"])
